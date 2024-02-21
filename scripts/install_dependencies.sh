ef50c2acca4b86f2de5d9073b229bc84d757342c
Asloob khan <147397491+asloobk@users.noreply.github.com>
2024-02-21 19:17:27 +0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+if ! command -v pip &> /dev/null; then
+    echo "Installing pip..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py
+    rm get-pip.py
+fi
 # Navigate to the directory where requirements.txt is located
 cd /home/ec2-user
 
